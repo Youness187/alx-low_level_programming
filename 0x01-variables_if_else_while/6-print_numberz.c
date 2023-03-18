9238b48a9487cf9f4b922a7c78ac4ca29d0e2245
ye <dayouness187@gmail.com>
2023-03-18 20:40:18 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	int i;
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
 
-	for (i = 0; i < 10; i++)
-		putchar(i + '0');
-	
-	putchar('\n');
-
-	return (0);
+return (0);
 }
