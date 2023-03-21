2b2410be85e23b7e837e049eaa4b1101014caab8
Youness187 <dayouness187@gmail.com>
2023-03-21 12:22:10 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,10 @@
  */
 int _isalpha(int c)
 {
-	int i = 0;
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-		i = 1;
-
-	return (i);
+int i = 0;
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+i = 1;
+}
+return (i);
 }
