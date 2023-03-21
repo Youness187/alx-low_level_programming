f6ecb41faf1a0c300291332faa879db75aa83c5a
Youness187 <dayouness187@gmail.com>
2023-03-21 21:59:44 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,9 +4,10 @@
  */
 void jack_bauer(void)
 {
-for (int i = 0; i < 24; i++)
+int i,j;
+for (i = 0; i < 24; i++)
 {
-for (int j = 0; j < 60; j++)
+for (j = 0; j < 60; j++)
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
