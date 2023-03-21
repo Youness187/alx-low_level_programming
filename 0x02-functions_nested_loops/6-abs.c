d7768669bc68e030970ebea8cbb344015e33e71b
Youness187 <dayouness187@gmail.com>
2023-03-21 18:03:34 +0000

    Abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/*
+/**
  * _abs - checks for absolute value
  * @n : parameter to be checked
- * Return: n 
+ * Return: n
  */
 int _abs(int n)
 {
 if (n < 0)
 {
-return -(n);
+return (-n);
 }
 else
 {
