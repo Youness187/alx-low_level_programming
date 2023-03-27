13d667fc414c4eafca89dcdbf526fe11317cdd4d
Youness187 <dayouness187@gmail.com>
2023-03-28 00:03:38 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; *s != '\0'; s++)
+	{
+		l++;
+	}
+	return (l);
+}
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: the variable
+ * @src: the string
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int leng = _strlen(src);
+	int i;
+
+	for (i = 0; i < leng; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
