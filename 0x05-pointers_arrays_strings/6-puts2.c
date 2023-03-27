37c1db043681b144ba0128ff49fd05e45fe2ad4b
Youness187 <dayouness187@gmail.com>
2023-03-27 23:13:42 +0100

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; *s != '\0'; s++)
+	{
+		l++;
+	}
+	return (l);
+}
+
+
+/**
  * puts2 - function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: string
@@ -7,9 +24,11 @@
 
 void puts2(char *str)
 {
+	int i = 0;
+
 	do {
-		_putchar(*str);
-		str += 2;
-	} while (*str != '\0');
+		_putchar(str[i]);
+		i += 2;
+	} while (str[i] != '\0' && i < _strlen(str));
 	_putchar('\n');
 }
