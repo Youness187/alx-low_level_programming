ef4985d614a6212f97a6709d8c630a4f994c3305
Youness187 <dayouness187@gmail.com>
2023-03-29 16:58:48 +0100

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,39 +1,25 @@
 #include "main.h"
 /**
- * check - chakes if a is an A or E or O or T or L
- * @a: char
- * Return: a
-*/
-
-char check(char a)
-{
-	int i;
-	char Char[] = "aAeEoOtTlL";
-	char Numbers[] = "43071";
-
-	for (i = 0; i < 8; i++)
-	{
-		if (a == Char[i])
-		{
-			a = Numbers[i / 2];
-			break;
-		}
-	}
-	return (a);
-}
-
-/**
  * leet - function that encodes a string into 1337.
  * @s: String
  * Return: String into 1337.
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char Char[] = "aAeEoOtTlL";
+	char Numbers[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		s[i] = check(s[i]);
+		for (j = 0; j < 8; j++)
+		{
+			if (s[i] == Char[j])
+			{
+				s[i] = Numbers[j / 2];
+				break;
+			}
+		}
 	}
 	return (s);
 }
