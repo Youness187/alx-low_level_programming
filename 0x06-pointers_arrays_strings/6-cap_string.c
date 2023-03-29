290188dd1893bffe745abc6f25cf41a4e59ced45
Youness187 <dayouness187@gmail.com>
2023-03-29 16:34:45 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * check - chakes if a is an A or E or O or T or L
+ * @a: char
+ * @b: char
+ * Return: a
+*/
+
+char check(char a, char b)
+{
+	int i;
+	char Char[] = "\n\t ,;.!?(){}\"";
+
+	for (i = 0; i < 13; i++)
+	{
+		if (b == Char[i])
+		{
+			a -= 32;
+			break;
+		}
+	}
+	return (a);
+}
+/**
  * cap_string - function that capitalizes all words of a string.
  * @s: String
  * Return: capitalized words of a string.
@@ -11,9 +33,9 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 97 && s[i] <= 122) && (s[i - 1] <= 32 && s[i - 1] >= 0))
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			s[i] -= 32;
+			s[i] = check(s[i], s[i - 1]);
 		}
 		i++;
 	}
