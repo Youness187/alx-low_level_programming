5ac6461137f966eb17e17f3895a86ed06e153b53
ye <dayouness187@gmail.com>
2023-04-03 14:24:34 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *s != '\0'; s++)
 	{
-		if (*(s + i) == c)
-			return (s + i);
+		if (*s == c)
+			return (s);
 	}
-	return (s + i);
+	return (s + 1);
 }
