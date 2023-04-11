043f9ba96c490713da00b3c37f1a80de3d4b4372
ye <dayouness187@gmail.com>
2023-04-11 14:39:15 +0100

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * length - method that return length of string
+ * @d: String
+ * Return: i length of string
+*/
+
+int length(char *d)
+{
+	int i;
+
+	for (i = 0; *d != '\0'; d++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	char *cp;
+	int i, l = length(str);
+
+	a = malloc(l + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++)
+		cp[i] = str[i];
+	cp[l + 1] = '\0';
+	return (cp);
+}
+
