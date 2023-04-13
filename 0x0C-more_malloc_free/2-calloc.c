561ce3df5e22990bcf8e332594bf901aa5ba4f9e
ye <dayouness187@gmail.com>
2023-04-13 12:50:10 +0100

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	n = malloc(nmemb * size);
 	if (n == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		n[i] = 0;
 
 	return (n);
