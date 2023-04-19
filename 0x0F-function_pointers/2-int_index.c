242f45cfe64ed187357ae10d413da4677432f76a
Youness187 <dayouness187@gmail.com>
2023-04-19 16:55:44 +0100

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer.
+ * @array: array of numbers
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: the index of the first element for which the cmp
+ * or If no element matches, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
+
