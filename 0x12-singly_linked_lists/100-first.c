71f066785fa3e96b12429d95e78ac5a95b32ca59
Youness187 <dayouness187@gmail.com>
2023-04-27 17:33:30 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * b_main - Print statement within this fxn before running main fxn.
+ */
+void __attribute__((constructor)) b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
