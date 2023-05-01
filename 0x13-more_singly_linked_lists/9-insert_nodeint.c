b3850a5dfdb1ae8524cabb8ebe38674662ddbff6
Youness187 <dayouness187@gmail.com>
2023-05-01 20:30:13 +0100

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,8 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 
-	new->next = h->next;
-	h->next = new;
+	if (idx == 0)
+	{
+		*head = new;
+		new->next = h;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
 
 	return (new);
 }
