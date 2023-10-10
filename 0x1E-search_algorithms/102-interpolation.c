a3e5874cb8b91373ccbe6b11b1f73026f5fb8c0e
younesse el-mouden <dayouness187@gmail.com>
2023-10-10 19:44:26 +0000

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,6 +11,9 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1, pos = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	do {
 		pos = low + (((double)(high - low) / (array[high] -
 					array[low])) * (value - array[low]));
@@ -27,8 +30,6 @@ int interpolation_search(int *array, size_t size, int value)
 			low = pos + 1;
 		if (array[pos] == value)
 			return (pos);
-		if (low == high)
-			break;
 	} while (array[pos] != value && pos < size);
 	return (-1);
 }
